ad60921ebe7a845301859284a5860df509258e53
sufan <sofarsogood001@163.com>
2024-12-02 00:27:51 +0800

    集成firebase-crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.google.servcie)
+  alias(libs.plugins.firebase.crashlytics)
 }
 
 android {
@@ -50,4 +52,7 @@ dependencies {
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
+
+  implementation(platform(libs.firebase.box))
+  implementation(libs.firebase.crashlytics)
 }
